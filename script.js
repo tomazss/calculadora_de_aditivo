91f4518f0e11614b4170aea580877b1248159d5e
tomazss <tomazsilva123@hotmail.com>
2021-05-15 16:26:41 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+ function calcular(){
+    
+    let total = window.document.querySelector('input#tnum')
+    let aditivo = window.document.getElementById('anum') 
+    let quantidade = window.document.getElementById('qnum') 
+    let res =window.document.getElementById('res')
+    let v1 = Number(total.value)
+    let v2 = Number(aditivo.value)
+    let v3 = Number(quantidade.value)
+     var resultado = v3 * v2 / v1
+     
+     
+     if ( v1>= v3) {
+        
+         
+         res.innerHTML = ''
+         res.innerHTML += ` <p>você tem que colocar <strong>${resultado}ml</strong> de estabilizador para combustível</p> `
+         
+     }else{
+        res.innerHTML = ''
+        res.innerHTML += ` <p> ERRO quantidade de litros informada  é maior é maior que a capacidae do tanque</p> `
+     }
+     
+
+    }
+  
+     
+       
+    
+       
+        
+    
+    
+    
+    
